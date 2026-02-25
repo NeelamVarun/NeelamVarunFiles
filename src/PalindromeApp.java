55f969f93c5587fcb22b4719ca385c1060f1efe7
NeelamVarun <varun30.neelam@gmail.com>
2026-02-25 16:09:39 +0530

    Base application setup and UC1

---
+++ src/PalindromeApp.java
@@ -0,0 +1,6 @@
+public class PalindromeApp {
+    public static void main(String[] args) {
+        // UC1: Welcome Message
+        System.out.println("--- Welcome to the Palindrome Checker App ---");
+    }
+}
