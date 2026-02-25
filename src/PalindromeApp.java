dbe9204574b79a853476c6f95b7c4e001b733411
NeelamVarun <varun30.neelam@gmail.com>
2026-02-25 16:14:00 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -2,5 +2,15 @@ public class PalindromeApp {
     public static void main(String[] args) {
         // UC1: Welcome Message
         System.out.println("--- Welcome to the Palindrome Checker App ---");
+
+        // UC2: Hardcoded Palindrome
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
